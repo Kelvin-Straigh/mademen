fc968b4471456ae8847ec9623d74ffcecf1ff1d7
kelvin straigh <kelvinstraigh@gmail.com>
2023-04-27 07:04:05 -0700

    minor

---
+++ egs.js
@@ -0,0 +1,57 @@
+/* let age = 89
+
+if(age<6){
+    console.log("free")
+}else if(age<18){
+    console.log("child discount")
+}else if(age<27){
+  console.log("student discount")
+}else if(age<67){
+console.log("full price")
+}else{
+    console.log("senior citizen")
+} */
+
+/* 
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+largeCountries.push("Ghana")
+for (let i=0; i < largeCountries.length; i++)
+
+console.log(largeCountries[i]) */
+/* 
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+largeCountries.pop("Monaco")
+largeCountries.push("Pakistan")
+
+console.log(largeCountries)
+
+
+largeCountries.shift("Tuvalu")
+largeCountries.unshift("China")
+
+console.log(largeCountries)   */
+
+/* 
+let dayofMonth = 31
+let weekday = "Friday"
+
+if(dayofMonth ===31 && weekday==="Friday")
+console.log("fear") */
+
+/* 
+let hands = ["rock","paper","scissor"]
+
+function getHands(){
+    let gethandsindex = Math.floor(Math.random()*3)
+    return hands[gethandsindex]
+}
+
+console.log(getHands())
+ */
+
+ 
+let phoneNumber = ["(012) 345-6789"]
+
+for (let i=0; i<phoneNumber.length; i++)
+
+console.log(phoneNumber[i])
